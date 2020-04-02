8da2c9577221098f6049d648fcee6e800005b7dc
Ali <ali.nili128@gmail.com>
2020-04-02 15:57:33 -0700

    Added firebase deployment files

--- js/run.js
+++ js/run.js
@@ -11,7 +11,7 @@ const UPDATE_TIMEOUT = 200;
 let canChangeDirection = true;
 
 function start() {
-    game = new Game(10, 10, 5, 7, 'UP');
+    game = new Game(10, 10, 5, 7, "UP");
     snake = game.snake;
     head = snake.head;
 
@@ -30,7 +30,7 @@ function start() {
 
     document.addEventListener("keydown", handleKeydown);
     ticker = setInterval(update, UPDATE_TIMEOUT);
-    
+
     // TODO: Set initial size of snake while making sure it stays inside the board
     snake.move();
     snake.append();
@@ -44,13 +44,20 @@ function start() {
 
 function update() {
     snake.move();
+
+    // check if the snake ate fruit in the last move. 
     let foodType = game.foodType(snake.head.x, snake.head.y);
-    if (foodType === 'GOOD') {
+    if (foodType === "GOOD") {
         snake.append();
         game.removeFood(snake.head.x, snake.head.y);
-    } else if (foodType === 'BAD') {
+    } else if (foodType === "BAD") {
         game.removeFood(snake.head.x, snake.head.y);
     }
+
+	// check if the player lost in the last move. 
+	if (snake.hasBitItself() || game.isSnakeOutOfBounds()) {
+		alert("You lost!");
+	}
 
     game.render();
     canChangeDirection = true;
@@ -58,52 +65,52 @@ function update() {
 
 function handleKeydown(event) {
     switch (event.code) {
-        case 'ArrowLeft':
-            changeDirection('LEFT');
+        case "ArrowLeft":
+            changeDirection("LEFT");
             break;
-        case 'ArrowRight':
-            changeDirection('RIGHT'); 
+        case "ArrowRight":
+            changeDirection("RIGHT");
             break;
-        case 'ArrowDown':
-            changeDirection('DOWN'); 
+        case "ArrowDown":
+            changeDirection("DOWN");
             break;
-        case 'ArrowUp':
-            changeDirection('UP'); 
+        case "ArrowUp":
+            changeDirection("UP");
             break;
     }
 }
 
 /**
- * Gets a string indicating the new direction of the snake. 
- * This function is called after pressing a keyboard key or 
- * after clicking an arrow-key button a mobile device. 
+ * Gets a string indicating the new direction of the snake.
+ * This function is called after pressing a keyboard key or
+ * after clicking an arrow-key button on a mobile device.
  */
 function changeDirection(dir) {
     if (!canChangeDirection) {
         return;
     }
     switch (dir) {
-        case 'LEFT':
-            if (head.direction !== 'RIGHT') {
-                head.setDirection('LEFT');
+        case "LEFT":
+            if (head.direction !== "RIGHT") {
+                head.setDirection("LEFT");
                 canChangeDirection = false;
             }
             break;
-        case 'UP':
-            if (head.direction !== 'DOWN') {
-                head.setDirection('UP');
+        case "UP":
+            if (head.direction !== "DOWN") {
+                head.setDirection("UP");
                 canChangeDirection = false;
             }
             break;
-        case 'RIGHT':
-            if (head.direction !== 'LEFT') {
-                head.setDirection('RIGHT');
+        case "RIGHT":
+            if (head.direction !== "LEFT") {
+                head.setDirection("RIGHT");
                 canChangeDirection = false;
             }
             break;
-        case 'DOWN':
-            if (head.direction !== 'UP') {
-                head.setDirection('DOWN');
+        case "DOWN":
+            if (head.direction !== "UP") {
+                head.setDirection("DOWN");
                 canChangeDirection = false;
             }
             break;
